9ed971df603bf34a8d2903f8c99b57bb78c9893d
Kancsár Csaba <csaba.kancsar@vereb.dc>
2019-11-05 17:56:33 +0100

    styel formázás

--- index.php
+++ index.php
@@ -8,15 +8,15 @@
     </style>
 </head>
 <body>
-    <div>
-        <div></div>
-        <div></div>
-        <div></div>
-        <div></div>
-        <div></div>
-        <div></div>
-        <div></div>
-        <div></div>
+    <div id="content">
+        <div id="a">1</div>
+        <div id="b">2</div>
+        <div id="c">3</div>
+        <div id="d">4</div>
+        <div id="e">5</div>
+        <div id="f">6</div>
+        <div id="g">7</div>
+        <div id="h">8</div>
     </div>
 </body>
 </html>
