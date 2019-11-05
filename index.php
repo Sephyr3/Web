0934ab2e669338544a141d8d62cc26c2164ba5fb
Kancsár Csaba <csaba.kancsar@vereb.dc>
2019-11-05 16:42:54 +0100

    fc

---
+++ index.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html lang="hu">
+<head>
+    <meta http-equiv="content-type" content="text/html" charset="UTF-8">
+    <title>Grid</title>
+</head>
+<body>
+    
+</body>
+</html>
